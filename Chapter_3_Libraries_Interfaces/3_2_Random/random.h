4e2f91f31841df165c7bdca4295506d5c3bee391
Oyashi <oyeshsin@hotmail.com>
2016-12-26 22:52:51 -0600

    Implementation of interface

---
+++ Chapter_3_Libraries_Interfaces/3_2_Random/random.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef __random_h__
+#define __random_h__
+int RandomInteger(int low, int high);
+double RandomReal(double low, double high);
+bool RandomChance(double p);
+void Randomize();
+#endif // !__random_h__
+
+
+
