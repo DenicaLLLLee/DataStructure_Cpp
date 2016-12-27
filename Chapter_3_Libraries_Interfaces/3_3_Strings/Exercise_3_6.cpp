040e81f2ed27cbf07ec360364d8d796be8cd683b
Oyashi <oyeshsin@hotmail.com>
2016-12-27 17:50:06 -0600

    This program checks if given word is palindrome

---
+++ Chapter_3_Libraries_Interfaces/3_3_Strings/Exercise_3_6.cpp
@@ -0,0 +1,24 @@
+/*
+A palindrome is a word that reads identically backward and forward, such as level
+or noon. Write a predicate function IsPalindrome(str) that returns true if the
+string str is a palindrome. In addition, design and write a test program that calls
+IsPalindrome to demonstrate that it works.
+12/27/2016
+Oyashi
+*/
+
+#include <iostream>
+#include "strutils.h"
+
+using namespace std;
+
+int main() {
+	string inputWord;
+	cout << "Enter the word "; cin >> inputWord;
+	if (IsPalindrome(inputWord)) {
+		cout << "Given word is palindrome" << endl;
+	}
+	else {
+		cout << "Given word is not palindrome" << endl;
+	}
+}
