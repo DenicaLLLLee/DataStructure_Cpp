a6e1b8a0259750f88ca49e97a7fc96f6360a942e
Oyashi <oyeshsin@hotmail.com>
2016-12-27 15:40:19 -0600

    This file simulates flipping of a coin

---
+++ Chapter_3_Libraries_Interfaces/3_2_Random/3_2.cpp
@@ -0,0 +1,35 @@
+/*
+	Write a program that simulates flipping a coin repeatedly and continues until three
+	consecutive heads are tossed. At that point, your program should display the total
+	number of coin flips that were made.
+	12/27/2016
+	Oyashi
+*/
+
+#include <iostream>
+#include "random.h"
+
+#define PROBABILITY 0.5
+
+using namespace std;
+
+int main() {
+	Randomize();
+
+	int count = 0;
+	int totalCounter = 0;
+
+	while(true) {
+		if (RandomChance(PROBABILITY)) {
+			cout << "Head" << endl;
+			count++;
+		}
+		else {
+			cout << "Tail" << endl;
+			count = 0;
+		}
+		if (count == 3) break;
+		totalCounter++;
+	}
+	cout << "It took " << totalCounter << " flips to get " << count << " consecutive heads." << endl;
+}
