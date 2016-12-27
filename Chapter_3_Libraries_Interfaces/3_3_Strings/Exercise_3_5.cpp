9362e64bb51108ef04e71f6b8053e34f519d6fa8
Oyashi <oyeshsin@hotmail.com>
2016-12-27 17:14:22 -0600

    This program capitalizes the first letter of given word

---
+++ Chapter_3_Libraries_Interfaces/3_3_Strings/Exercise_3_5.cpp
@@ -0,0 +1,20 @@
+/*
+Implement a function Capitalize(str) that returns a string in which the initial
+character is capitalized (if it is a letter) and all other letters are converted so that they
+appear in lowercase form. Characters other than letters are not affected. For
+example, Capitalize("BOOLEAN") and Capitalize("boolean") should each
+return the string "Boolean".
+Oyashi
+12/27/2016
+*/
+
+#include <iostream>
+#include "strutils.h"
+
+using namespace std;
+
+int main() {
+	string inputWord;
+	cout << "Enter your word in random case "; cin >> inputWord;
+	cout << "After capitalizing " << Capitalize(inputWord) << endl;
+}
