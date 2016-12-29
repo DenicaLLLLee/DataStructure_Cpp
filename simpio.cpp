e2797c73a8a09d7befaf82494a872b82c614d595
Oyashi <oyeshsin@hotmail.com>
2016-12-28 19:04:33 -0600

    Added simpio interface

---
+++ simpio.cpp
@@ -0,0 +1,29 @@
+#include "simpio.h"
+
+int GetInteger()
+{
+	int input;
+	std::cin >> input;
+	return input;
+}
+
+long GetLong()
+{
+	long input;
+	cin >> input;
+	return input;
+}
+
+double GetReal()
+{
+	double input;
+	cin >> input;
+	return input;
+}
+
+string GetLine()
+{
+	string input;
+	getline(cin, input);
+	return input;
+}
