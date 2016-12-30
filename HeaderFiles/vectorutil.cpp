1967ff0c6d2fae9bf527de4f873af72f979b4903
Oyashi <oyeshsin@hotmail.com>
2016-12-29 23:51:30 -0600

    This implementation file can still be used for int type only for now

--- HeaderFiles/vectorutil.cpp
+++ HeaderFiles/vectorutil.cpp
@@ -6,11 +6,44 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include "HeaderFiles\strutils.h"
 #include "vectorutil.h"
 
 using namespace std;
 
-void DisplayVector(vector<int> & vec) {
+/*
+Function: RemoveZeroElements
+Usage: RemoveZeroElements(vec);
+------------------------------
+This function removes ZERO elements
+from the given vector.
+*/
+void RemoveZeroElements(std::vector<int> & vec) {
+	vec.erase(remove(vec.begin(), vec.end(), 0), vec.end());
+	std::cout << "After Zero Removal" << std::endl;
+	DisplayVector(vec);
+}
+/*
+Function: RemoveSpecificElement
+Usage: RemoveSpecificElement(vec);
+------------------------------
+This function removes any user given
+elements from the given vector.
+*/
+void RemoveSpecificElement(std::vector<int> & vec, int element) {
+	vec.erase(remove(vec.begin(), vec.end(), element), vec.end());
+	std::cout << "After " << element << " Removal" << std::endl;
+	DisplayVector(vec);
+}
+/*
+Function: DisplayVector
+Usage: DisplayVector(vec);
+------------------------------
+This function displays the elements
+from the given vector.
+*/
+void DisplayVector(std::vector<int> & vec) {
 	std::vector<int>::iterator itr;
 	for (itr = vec.begin(); itr != vec.end(); itr++) {
 		std::cout << *itr << " ";
@@ -18,14 +51,26 @@ void DisplayVector(vector<int> & vec) {
 	std::cout << std::endl;
 }
 
-void RemoveZeroElements(vector<int> & vec) {
-	vec.erase(remove(vec.begin(), vec.end(), 0), vec.end());
-	std::cout << "After Zero Removal" << std::endl;
-	DisplayVector(vec);
-}
-
-void RemoveSpecificElement(vector<int> & vec, int element) {
-	vec.erase(remove(vec.begin(), vec.end(), element), vec.end());
-	std::cout << "After " << element << " Removal" << std::endl;
-	DisplayVector(vec);
+bool ReadVector(ifstream & infile, vector<int> & vec) {
+	string line = "";
+	vec.clear();
+	while (infile.good())
+	{
+		streampos prevPos = infile.tellg();
+		getline(infile, line);
+		if (line == "") {
+			return true;
+		}
+		else {
+			getline(infile, line);
+			if (!infile.eof()) {
+				infile.seekg(prevPos);
+				getline(infile, line);
+				vec.push_back(StringToInteger(line));
+			}
+			else
+				return true;
+		}
+	}
+	return false;
 }
