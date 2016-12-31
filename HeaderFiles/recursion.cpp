df186a17574a24eee0f57cb293948648affc1747
Oyashi <oyeshsin@hotmail.com>
2016-12-31 16:04:12 -0600

    All recursive functions can be found here

---
+++ HeaderFiles/recursion.cpp
@@ -0,0 +1,34 @@
+#include "recursion.h"
+
+int factorial(int n) {
+	int prod = 1;
+	if (n == 0) {
+		return prod;
+	}
+	else {
+		prod = n * factorial(n - 1);
+	}
+}
+
+int fibonacci(int n) {
+	int sum = 0;
+	if (n == 1 || n == 0) {
+		return n;
+	}
+	else if (n < 0) {
+		return 0;
+	}
+	else {
+		return(fibonacci(n - 1) + fibonacci(n - 2));
+	}
+}
+
+int EfficientFibonacci(int n) {
+	return AdditiveSquence(n, 0, 1);
+}
+
+int AdditiveSquence(int n, int t0, int t1) {
+	if (n == 0) return t0;
+	if (n == 1) return t1;
+	return AdditiveSquence(n - 1, t1, t0 + t1);
+}
---
+++ HeaderFiles/recursion.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef __recursion_h__
+#define __recursion_h__
+int factorial(int n);
+int fibonacci(int n);
+int EfficientFibonacci(int n);
+int AdditiveSquence(int n, int t0, int t1);
+#endif // !__recursion_h__
+
+
