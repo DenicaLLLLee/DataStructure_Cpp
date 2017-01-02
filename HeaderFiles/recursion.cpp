e38cf641ed99a267d786920be0493022e00a191f
Oyashi <oyeshsin@hotmail.com>
2017-01-02 17:24:05 -0600

    Added PalindromeCheck function

--- HeaderFiles/recursion.cpp
+++ HeaderFiles/recursion.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include "recursion.h"
 
 int factorial(int n) {
@@ -31,4 +32,19 @@ int AdditiveSquence(int n, int t0, int t1) {
 	if (n == 0) return t0;
 	if (n == 1) return t1;
 	return AdditiveSquence(n - 1, t1, t0 + t1);
+}
+
+bool PalindromeCheck(std::string str) {
+	if (str.length() == 0 || str.length() == 1) {
+		return true;
+	}
+	else {
+		if (str[0] == str[str.length() - 1]) {
+			PalindromeCheck(str.substr(1, str.length() - 2));
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
 }
--- HeaderFiles/recursion.h
+++ HeaderFiles/recursion.h
@@ -5,6 +5,7 @@ int factorial(int n);
 int fibonacci(int n);
 int EfficientFibonacci(int n);
 int AdditiveSquence(int n, int t0, int t1);
+bool PalindromeCheck(std::string s);
 #endif // !__recursion_h__
 
 
