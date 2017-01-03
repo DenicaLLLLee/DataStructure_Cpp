c6e91d1e44d5124bc87903f789686cccec5539a1
Oyashi <oyeshsin@hotmail.com>
2017-01-03 14:30:19 -0600

    Added BinarySearch and FindStringInSortedArray

--- HeaderFiles/recursion.cpp
+++ HeaderFiles/recursion.cpp
@@ -48,3 +48,41 @@ bool PalindromeCheck(std::string str) {
 	}
 	return true;
 }
+
+/*
+* Function: FindStringInSortedArray
+* Usage: index = FindStringInSortedArray(key, array, n);
+* ------------------------------------------------------
+* This function searches the array looking for the specified
+* key. The argument n specifies the effective size of the
+* array, which must be sorted according to lexicographic
+* order. If the key is found, the function returns the
+* index in the array at which that key appears. (If the key
+* appears more than once in the array, any of the matching
+* indices may be returned). If the key does not exist in
+* the array, the function returns -1. In this implementation,
+* FindStringInSortedArray is simply a wrapper; all the work
+* is done by the recursive function BinarySearch.
+*/
+int FindStringInSortedArray(std::string key, std::string array[], int n) {
+	return BinarySearch(key, array, 0, n - 1);
+}
+/*
+* Function: BinarySearch
+* Usage: index = BinarySearch(key, array, low, high);
+* ---------------------------------------------------
+* This function does the work for FindStringInSortedArray.
+* The only difference is that BinarySearch takes both the
+* upper and lower limit of the search.
+*/
+int BinarySearch(std::string key, std::string array[], int low, int high) {
+	if (low > high) return -1;
+	int mid = (low + high) / 2;
+	if (key == array[mid]) return mid;
+	if (key < array[mid]) {
+		return BinarySearch(key, array, low, mid - 1);
+	}
+	else {
+		return BinarySearch(key, array, mid + 1, high);
+	}
+}
--- HeaderFiles/recursion.h
+++ HeaderFiles/recursion.h
@@ -6,6 +6,8 @@ int fibonacci(int n);
 int EfficientFibonacci(int n);
 int AdditiveSquence(int n, int t0, int t1);
 bool PalindromeCheck(std::string s);
+int FindStringInSortedArray(std::string key, std::string array[], int n);
+int BinarySearch(std::string key, std::string array[], int low, int high);
 #endif // !__recursion_h__
 
 
