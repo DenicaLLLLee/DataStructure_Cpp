ac21706a28891a89f691737125f6b82e45515f36
Oyashi <oyeshsin@hotmail.com>
2017-01-04 15:48:44 -0600

    This program calculates the digit root of the given integer

---
+++ Chapter_5_Recursion/Exercise_5_7.cpp
@@ -0,0 +1,28 @@
+/*
+The digital root of an integer n is defined as the result of summing the digits
+repeatedly until only a single digit remains. For example, the digital root of 1729
+can be calculated using the following steps:
+Step 1: 1 + 7 + 2 + 9 % 19
+Step 2: 1 + 9 % 10
+Step 3: 1 + 0 % 1
+Introduction to Recursion – 199 –
+Because the total at the end of step 3 is the single digit 1, that value is the digital
+root.
+Write a function DigitalRoot(n) that returns the digital root of its argument.
+Although it is easy to implement DigitalRoot using the DigitSum function from
+exercise 6 and a while loop, part of the challenge of this problem is to write the
+function recursively without using any explicit loop constructs.
+Oyashi
+01/04/2017
+*/
+
+#include <iostream>
+#include "HeaderFiles\recursion.h"
+
+using namespace std;
+
+int main() {
+	int input;
+	cout << "Please enter the number to get the root of its digits : "; cin >> input;
+	cout << "Final root = " << DigitRoot(input) << endl;
+}
