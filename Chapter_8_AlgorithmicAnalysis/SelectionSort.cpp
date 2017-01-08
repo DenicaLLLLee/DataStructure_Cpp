2629fc3b936054659cdead829c8991acd738828e
Oyashi <oyeshsin@hotmail.com>
2017-01-07 19:49:42 -0600

    Implementation of selection sort

---
+++ Chapter_8_AlgorithmicAnalysis/SelectionSort.cpp
@@ -0,0 +1,27 @@
+/*
+	Implementation of selection sort algorithm
+	Oyashi
+	01/07/2017
+*/
+
+#include <iostream>
+#include <vector>
+#include "HeaderFiles\sorting.h"
+
+using namespace std;
+
+int main() {
+	vector<int> inputArray = { 12,43,76,9,65,23,11,87 };
+	vector<int>::iterator itr = inputArray.begin();
+	cout << "Before Sorting" << endl;
+	for (itr = inputArray.begin(); itr != inputArray.end(); ++itr) {
+		cout << *itr << " ";
+	}
+	cout << endl;
+	SelectionSort(inputArray);
+	cout << "After Sorting" << endl;
+	for (itr = inputArray.begin(); itr != inputArray.end(); ++itr) {
+		cout << *itr << " ";
+	}
+	cout << endl;
+}
